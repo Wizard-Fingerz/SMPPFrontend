9933c7ba5ce86952939de00987270bdf83c3b8e8
Wizard-Fingerz <adewale.oladiti28@gmail.com>
2024-07-23 17:09:30 +0100

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['wizcode28.pythonanywhere.com'], // Add your image domain(s) here
+    },
+    reactStrictMode: true, // Enables React strict mode for additional checks and warnings
+    // Other Next.js configuration options can be added here
+  };
+  
+  export default nextConfig;
+  
