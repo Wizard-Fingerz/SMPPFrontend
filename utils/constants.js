52007e4180dcbbaa63ecd2bd2a34c79bc8717c8f
Wizard-Fingerz <adewale.oladiti28@gmail.com>
2024-07-24 18:42:37 +0100

    updated

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://127.0.0.1:8002';
-// export const API_BASE_URL = 'https://wizcode28.pythonanywhere.com/';
+// export const API_BASE_URL = 'http://127.0.0.1:8002';
+export const API_BASE_URL = 'https://wizcode28.pythonanywhere.com/';
